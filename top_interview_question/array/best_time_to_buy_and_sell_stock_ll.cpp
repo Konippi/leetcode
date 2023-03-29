cccd9c6de910acf3ee41907828d01bfca0879d6d
Konippi <konippi-114@outlook.jp>
2023-03-29 23:18:54 +0900

    completed array-2

--- top_interview_question/array/best_time_to_buy_and_sell_stock_ll.cpp
+++ top_interview_question/array/best_time_to_buy_and_sell_stock_ll.cpp
@@ -4,13 +4,12 @@ using namespace std;
 class Solution {
   public:
     int maxProfit(vector<int> &prices) {
-        int total = 0, nxt = 1;
-        for (int i = 0; i < prices.size() - 1; i++) {
-            if (prices[i] < prices[i + nxt]) {
-                total += prices[i + nxt] - prices[i];
-            } else {
-                nxt++;
+        int total = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1]) {
+                total += prices[i] - prices[i - 1];
             }
         }
+        return total;
     }
 };
