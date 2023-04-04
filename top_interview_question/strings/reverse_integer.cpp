05bde94bb3e9d542284f7dc7986c352403567c89
Konippi <konippi-114@outlook.jp>
2023-04-04 23:38:37 +0900

    completed strings-6

--- top_interview_question/strings/reverse_integer.cpp
+++ top_interview_question/strings/reverse_integer.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 class Solution {
   public:
-    long long reverse(int x) {
+    int reverse(int x) {
         bool isMinus = x < 0;
         string s = to_string(abs(x));
         ::reverse(s.begin(), s.end());
-        long long sNum = stol(s);
-        if (sNum > 2147483648) {
+        long long sNum = stoll(s);
+        if (sNum > INT_MAX) {
             return 0;
         }
-        return isMinus ? sNum - 2 * sNum : sNum;
+        return isMinus ? (int)(sNum - 2 * sNum) : int(sNum);
     }
 };
