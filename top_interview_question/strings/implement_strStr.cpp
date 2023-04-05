a64353a8b240fee187e4996fa1b550b0b77e1ab9
Konippi <konippi-114@outlook.jp>
2023-04-05 22:26:38 +0900

    completed strings-7

---
+++ top_interview_question/strings/implement_strStr.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int strStr(string haystack, string needle) {
+        int ans = -1;
+        if (haystack.find(needle) != string::npos) {
+            ans = haystack.find(needle);
+        }
+        return ans;
+    }
+};
