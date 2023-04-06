1740b8d79731f8da2de16d63449d0a3e9755e86f
Konippi <konippi-114@outlook.jp>
2023-04-06 23:00:07 +0900

    completed linkedList-3

---
+++ top_interview_question/linkedList/reverseLinkedList.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+  public:
+    ListNode *reverseList(ListNode *head) {
+        ListNode *pre = nullptr, *cur = head, *nxt;
+        while (cur != nullptr) {
+            nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+};
